ac40fb200ed79631cbc6874a73de3c30af79fc16
ian <kulickian@gmail.com>
2020-12-26 21:49:25 +0100

    geleert

--- src/com/chat/core/Main.java
+++ src/com/chat/core/Main.java
@@ -4,8 +4,5 @@ public class Main {
     public static void main(String args[]) {
         System.out.println("bobidi boobs");
     }
-    //test
-    //hallo
- // boooooooobbbbbsss
-        // hier ist ian
+
 }
