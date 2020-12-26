91f316fb3f578add1c7acb02968404e9184c0a83
ian <kulickian@gmail.com>
2020-12-26 22:40:23 +0100

    clean up

--- src/com/chat/core/Main.java
+++ src/com/chat/core/Main.java
@@ -3,10 +3,6 @@ package com.chat.core;
 public class Main {
     public static void main(String args[]) {
 
-        for (int i = 0; i <= 15; i++){
-            System.out.println(i);
-            i++;
-            //huhu
         }
     }
 }
