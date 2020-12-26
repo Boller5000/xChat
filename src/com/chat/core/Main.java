454108931cc00b3c2878cc98d74c665d1b07c4d3
Peter <HerrPeterLang@t-online.de>
2020-12-26 21:44:35 +0100

    blob

--- src/com/chat/core/Main.java
+++ src/com/chat/core/Main.java
@@ -2,9 +2,9 @@ package com.chat.core;
 
 public class Main {
     public static void main(String args[]) {
-
+        System.out.println("boobs");
     }
     //test
     //hallo
-        // hier ist ian
+        // hier war ian
 }
