9e79bf1c660f8a790ba60279cfb3384180d30c3c
ian <kulickian@gmail.com>
2020-12-26 22:10:25 +0100

    for schleife

--- src/com/chat/core/Main.java
+++ src/com/chat/core/Main.java
@@ -3,6 +3,9 @@ package com.chat.core;
 public class Main {
     public static void main(String args[]) {
         System.out.println("bobidi boobs");
+        for (int i=1; i <=15; i++){
+            System.out.print(i);
+            i = i +1;
+        }
     }
-
 }
