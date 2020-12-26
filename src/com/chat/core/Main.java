f25b0c3da9ef6bdaac1b9c8c066bfec6c6cf28cf
ian <kulickian@gmail.com>
2020-12-26 22:36:05 +0100

    brüste

--- src/com/chat/core/Main.java
+++ src/com/chat/core/Main.java
@@ -1,11 +1,10 @@
 package com.chat.core;
-
 public class Main {
     public static void main(String args[]) {
-        System.out.println("bobidi boobs");
+        System.out.println("ich mag brüste");
         for (int i=1; i <=15; i++){
             System.out.print(i);
-            i = i +1;
+            i = i +6;
         }
     }
 }
