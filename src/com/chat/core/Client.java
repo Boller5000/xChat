2538033c68162722ecd4af96fd3b5ad256cd280e
ian <kulickian@gmail.com>
2020-12-28 18:11:56 +0100

    korrektur code + commands

--- src/com/chat/core/Client.java
+++ src/com/chat/core/Client.java
@@ -6,14 +6,21 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.InputStreamReader;
 import java.net.*;
+import javax.swing.JFrame;
 
 public class Client {
     public Client() {
         try {
+            JFrame ui = new JFrame("xChat");
+            ui.setSize(500, 500);
+            ui.setVisible(true);
+            JLabel test = new JLabel("king",JLabel.CENTER);
+            ui.add(test);
+            test.setVerticalAlignment(JLabel.TOP);
+            test.setHorizontalTextPosition(JLabel.LEFT);
+
             Socket client = new Socket("217.81.193.203", 187);
             System.out.println("Connected to: " + client.getRemoteSocketAddress());
-
-            String i = client.getRemoteSocketAddress().toString();
 
             //Eingang festlegen
             BufferedReader eingangUser = new BufferedReader(new InputStreamReader(System.in));
@@ -23,27 +30,27 @@ public class Client {
             DataInputStream eingang = new DataInputStream(client.getInputStream());
 
             // parallelen Thread aufrufen und gucken ob es eine Nachricht gibt
-            new Thread(()->{
-                while(true) {
+            new Thread(() -> {
+                while (true) {
                     try {
                         String result = eingang.readUTF();
                         System.out.println(result);
-                    }
-                    catch (Exception e) {
+                    } catch (Exception e) {
                     }
                 }
             }).start();
             //Zeile einlesen
-            while(true) {
-            System.out.println("Nachricht eingeben:");
+            while (true) {
+                System.out.println("Nachricht eingeben:");
                 String s1 = eingangUser.readLine();
-                if(s1.equals("/exit")){
+                if (s1.equals("/exit")) {
                     System.exit(0);
                 }
                 ausgang.writeUTF(s1);
             }
-        } catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+
     }
 }
