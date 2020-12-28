22dc5670e373a83fb128f09b626bc866d44c8d0b
Peter <HerrPeterLang@t-online.de>
2020-12-28 16:46:14 +0100

    Double message deleted

--- src/com/chat/core/Server.java
+++ src/com/chat/core/Server.java
@@ -56,31 +56,68 @@ public class Server {
         }
         @Override
         public void run() {
+            //Beginn des Threads für die connection
           broadcast("Just connected to " + client.getRemoteSocketAddress() + " as client");
           while(clientName == socketAdress){
               try {
-                  sendMessage(this, "Bitte gib deinen Name ein");
-                  String input = in.readUTF();
-                  if (input.length() < 3) {
+                      sendMessage(this, "Bitte gib deinen Name ein");
+                      String input = in.readUTF();
+                      if (input.length() < 3) {
+                          sendMessage(this, "Dein name ist zu Kurz oder ist schon vergeben!");
+                          continue;
+                      }
+                      clientName = input;
+                      break;
 
-                      continue;
-                  }
-                  clientName = input;
+              } catch(Exception e){
+                  //Bei einem fehler wird der client gekickt
+                  try {
+                      client.close();
+                      connections.remove(this);
+                      serverBroadcast("Da konnte jemand seinen Namen nicht eingeben - unbekannter disconnected");
+                  } catch(Exception er){}
                   break;
-              } catch(Exception e){}
+              }
           }
-            while(true) {
-                    try{
-                        String message = "[" + clientName + "]: " + in.readUTF();
+          //Der Chat wird nur betreten wenn der client auch noch drinne ist, sollte dies nicht der falls sein, wird er aus der connections liste entfernt
+          if(connections.contains(this)) {
+              while (true) {
+                  try {
+                      String message = in.readUTF();
+                      String[] messageA = message.split(" ");
+                      switch (messageA[0]){
+                          case "/msg": {
+                              message = message.replace(messageA[0],"");
+                              message = message.replace(messageA[1],"");
+                              sendMessage(getConnectionByClientName(messageA[1]), "[flüstern von " + clientName + "]: " + message);
+                              continue;
+                          }
+                          case "/name" : sendMessage(this,this.clientName);continue;
+                          case "/help": sendMessage(this,"Command List: help,msg,name,ping");continue;
+                          case "/ping" : sendMessage(this,"pong");continue;
 
-                        broadcast(message);
-                    } catch(SocketException se) {
-                        break;
-                    }catch(Exception e) {
-                        e.printStackTrace();
-                        continue;
-                    }
-                }
+                          default: {
+                              message = "[" + clientName + "]: " + message;
+                              broadcast(message);
+
+                          }
+                      }
+
+
+                  }catch(ArrayIndexOutOfBoundsException aobe) {
+                      try {
+                          sendServerMessage(this, "fehlerhafte naricht");
+                      } catch(Exception e){}
+                  } catch (Exception se) {
+                      try {
+                          client.close();
+                          connections.remove(this);
+                          serverBroadcast(clientName + " hat den chat verlassen");
+                      } catch (Exception e) {}
+                      break;
+                  }
+              }
+          }
         }
 
     }
@@ -92,23 +129,37 @@ public class Server {
      */
     private void broadcast(String message) {
         System.out.println(broadcast_string + message);
-        try {
 
 
             for (Connection s : connections) {
-                s.out.writeUTF(message);
+                try {
+                    if(!message.contains("["+s.clientName + "]"))
+                    s.out.writeUTF(message);
+                }catch(IOException e) {
+                        System.err.println("IOException while Broadcasting(Connections lenght: " + connections.size() + ")");
+                }
 
-            }
-        } catch(IOException e) {
-            System.err.println("IOException while Broadcasting");
         }
     }
-    private void sendMessage(Connection client,String message) {
-        System.out.println("[->" + client.clientName + "]: " + message);
-        try {
-            client.out.writeUTF(message);
-        } catch(IOException e) {
-            System.err.println("IOException while sendingMessage");
+    private void sendMessage(Connection client,String message) throws IOException {
+            System.out.println("[->" + client.clientName + "]: " + message);
+                client.out.writeUTF(message);
+
+    }
+
+    public void serverBroadcast(String message) {
+        broadcast("[Server]: " + message);
+    }
+    public void sendServerMessage(Connection client,String message) throws IOException{
+        sendMessage(client,"[Server]: " + message);
+    }
+
+    public Connection getConnectionByClientName(String name) throws Exception{
+        for(Connection c : connections) {
+            if(c.clientName.contains(name) ) {
+                return c;
+            }
         }
+        throw new Exception();
     }
 }
